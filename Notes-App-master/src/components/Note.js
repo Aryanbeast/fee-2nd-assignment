8930527efa556c930cad0c71926b99ab5f504a45
Aryan <123055289+Aryanbeast@users.noreply.github.com>
2024-04-19 20:37:19 +0530

    Add files via upload
    this is the first deploy

---
+++ Notes-App-master/src/components/Note.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Note = ({ id, text, date, handleDeleteNote }) => {
+    return (
+        <div className='note'>
+            <span>{text}</span>
+            <div className='note-footer'>
+                <small>{date}</small>
+                <button onClick={() => handleDeleteNote(id)} className='delete-icon'>
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Note;
